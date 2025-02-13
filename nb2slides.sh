2f7d1228e36fa278df859db0446bf6ced962bda7
andrecastro0o <andre.castro@dans.knaw.nl>
2025-02-13 14:29:46 +0100

    .sh script uncommented slides

--- nb2slides.sh
+++ nb2slides.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# jupyter nbconvert $1 --to slides --SlidesExporter.reveal_scroll=True
+jupyter nbconvert $1 --to slides --SlidesExporter.reveal_scroll=True
 jupyter nbconvert $1 --to webpdf --allow-chromium-download
-# pdf: requires xelatex debian/ubuntu package: texlive-xetex
